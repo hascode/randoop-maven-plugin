a29bbbddb7b04e72191082a5c00aee76bfff3f67
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2012-11-30 13:59:08 -0600

    Remove unused import.

--- src/main/java/net/sourceforge/javydreamercsw/randoop/mojo/GenerateAllTestsMojo.java
+++ src/main/java/net/sourceforge/javydreamercsw/randoop/mojo/GenerateAllTestsMojo.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
